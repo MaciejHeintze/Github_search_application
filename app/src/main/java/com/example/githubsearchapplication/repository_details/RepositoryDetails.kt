b94dcb79571c36b778b721b3bf94ccf9c4d7d77a
Maciek <maciej.heintze96@gmail.com>
2020-03-10 18:44:16 +0100

    network state and single repo commit

---
+++ app/src/main/java/com/example/githubsearchapplication/repository_details/RepositoryDetails.kt
@@ -0,0 +1,13 @@
+package com.example.githubsearchapplication.repository_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubsearchapplication.R
+
+class RepositoryDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_repository_details)
+    }
+}
